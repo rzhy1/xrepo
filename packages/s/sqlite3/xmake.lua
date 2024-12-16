c7059f9457f9d97a72cef14c7cb418babb249f78
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-16 23:02:54 +0800

    Update xmake.lua

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -3,8 +3,20 @@ package("sqlite3")
     set_description("SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine.")
     set_license("MIT")
 
-    -- 固定下载地址
-    set_urls("https://sqlite.org/2024/sqlite-autoconf-3470200.tar.gz", {verify = false})
+    -- 直接使用固定下载地址
+    set_urls("https://sqlite.org/2024/sqlite-autoconf-3470200.tar.gz")
+
+    -- 仅保留最新版本
+    add_versions("3.47.0+200", "f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b")
+
+    add_configs("explain_comments", { description = "Inserts comment text into the output of EXPLAIN.", default = true, type = "boolean"})
+    add_configs("dbpage_vtab",      { description = "Enable the SQLITE_DBPAGE virtual table.", default = true, type = "boolean"})
+    add_configs("stmt_vtab",        { description = "Enable the SQLITE_STMT virtual table logic.", default = true, type = "boolean"})
+    add_configs("dbstat_vtab",      { description = "Enable the dbstat virtual table.", default = true, type = "boolean"})
+    add_configs("math_functions",   { description = "Enable the built-in SQL math functions.", default = true, type = "boolean"})
+    add_configs("rtree",            { description = "Enable R-Tree.", default = false, type = "boolean"})
+    add_configs("safe_mode",        { description = "Use thread safe mode in 0 (single thread) | 1 (serialize) | 2 (mutli thread).", default = "1", type = "string", values = {"0", "1", "2"}})
+    add_configs("cli",              { description = "Build the sqlite3 command line shell.", default = false, type = "boolean"})
 
     on_install(function (package)
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
