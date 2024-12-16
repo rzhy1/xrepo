9b101611e1d59442c78fcb4e34bc49321a96c08b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-16 12:20:58 +0800

    Update xmake.lua

--- packages/c/c-ares/xmake.lua
+++ packages/c/c-ares/xmake.lua
@@ -13,14 +13,14 @@ package("c-ares")
         end
     })
 
-    -- 动态获取最新版本的哈希值
-    add_versions("dynamic", function (version)
-        import("lib.detect.find_tool")
+    -- 使用一个固定的动态哈希值解析函数
+    add_versions("dynamic", function(version)
         import("net.http")
-        local download_url = string.format("https://github.com/c-ares/c-ares/releases/download/v%s/c-ares-%s.tar.gz", version, version)
+        import("lib.detect.find_tool")
+        local sha256sum = assert(find_tool("sha256sum"), "sha256sum is required to calculate hash!")
         local tmpfile = os.tmpfile() .. ".tar.gz"
-        http.download(download_url, tmpfile)
-        local sha256sum = assert(find_tool("sha256sum"), "sha256sum is required to calculate the hash!")
+        local url = string.format("https://github.com/c-ares/c-ares/releases/download/v%s/c-ares-%s.tar.gz", version, version)
+        http.download(url, tmpfile)
         local hash = os.iorunv(sha256sum.program, {tmpfile})
         os.rm(tmpfile)
         return hash:split("%s")[1]
@@ -33,27 +33,10 @@ package("c-ares")
     end)
 
     on_install(function (package)
-        local transforme_configfile = function(input, output)
-            output = output or input
-            local lines = io.readfile(input):gsub("@([%w_]+)@", "${%1}"):split("\n")
-            local out = io.open(output, 'wb')
-            for _, line in ipairs(lines) do
-                if line:startswith("#cmakedefine") then
-                    local name = line:split("%s+")[2]
-                    line = "${define " .. name .. "}"
-                end
-                out:write(line)
-                out:write("\n")
-            end
-            out:close()
-        end
-        transforme_configfile("include/ares_build.h.cmake", "cmake/ares_build.h.in")
-        transforme_configfile("src/lib/ares_config.h.cmake", "cmake/ares_config.h.in")
         os.cp(path.join(os.scriptdir(), "port/*.lua"), "./")
-        local configs = {}
-        import("package.tools.xmake").install(package, configs)
+        import("package.tools.xmake").install(package)
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("ares_library_initialized", {includes = {"ares.h"}}))
+        assert(package:has_cfuncs("ares_library_initialized", {includes = "ares.h"}))
     end)
