9f3423cfc6c860e263ca5ede91a19e66542b2a55
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-16 23:32:03 +0800

    Update xmake.lua

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -2,63 +2,29 @@ package("sqlite3")
     set_homepage("https://sqlite.org/index.html")
     set_description("SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine.")
     set_license("MIT")
-    
-    local version = nil
-    local download_url = nil
-     
-    local function get_download_url(version)
-       return "https://www.sqlite.org/2024/sqlite-autoconf-" .. version:gsub("[.+]", "") .. "00.tar.gz"
-    end
+
+    -- 固定年份为最新版本的年份 2024
+    set_urls("https://sqlite.org/2024/sqlite-autoconf-$(version).tar.gz", {version = function (version)
+        local version_str = version:gsub("[.+]", "")
+        if #version_str < 7 then
+            version_str = version_str .. "00"
+        end
+        return version_str
+    end})
+
+    -- 仅保留最新版本
+    add_versions("3.47.0+200", "f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b")
+
+    add_configs("explain_comments", { description = "Inserts comment text into the output of EXPLAIN.", default = true, type = "boolean"})
+    add_configs("dbpage_vtab",      { description = "Enable the SQLITE_DBPAGE virtual table.", default = true, type = "boolean"})
+    add_configs("stmt_vtab",        { description = "Enable the SQLITE_STMT virtual table logic.", default = true, type = "boolean"})
+    add_configs("dbstat_vtab",      { description = "Enable the dbstat virtual table.", default = true, type = "boolean"})
+    add_configs("math_functions",   { description = "Enable the built-in SQL math functions.", default = true, type = "boolean"})
+    add_configs("rtree",            { description = "Enable R-Tree.", default = false, type = "boolean"})
+    add_configs("safe_mode",        { description = "Use thread safe mode in 0 (single thread) | 1 (serialize) | 2 (mutli thread).", default = "1", type = "string", values = {"0", "1", "2"}})
+    add_configs("cli",              { description = "Build the sqlite3 command line shell.", default = false, type = "boolean"})
 
     on_install(function (package)
-        local function get_sqlite_info()
-            local retry = function(cmd)
-              local output, code = os.runv(cmd)
-              if code == 0 then
-                return output
-              end
-              return nil
-            end
-            local index_page = retry{"curl", "-s", "https://www.sqlite.org/index.html"}
-            if not index_page then
-              return nil, nil
-            end
-            
-            local version = index_page:match(">Version ([0-9.]+)<")
-            
-            local download_page = retry{"curl", "-s", "https://www.sqlite.org/download.html"}
-            if not download_page then
-              return nil, nil
-            end
-            
-            local csv_data = download_page:match("Download product data for scripts to read(.*)-->")
-            if not csv_data then
-               return nil,nil
-            end
-            local tarball_url = csv_data:match("autoconf.*%.tar%.gz")
-            if not tarball_url then
-               return nil, nil
-            end
-            local download_url = "https://www.sqlite.org/" .. tarball_url
-            
-            return version, download_url
-          end
-          
-        version, download_url = get_sqlite_info()
-        if not version then
-            print("Failed to get the latest version of sqlite3, using default version 3.47.0")
-           version = "3.47.0"
-           download_url = "https://www.sqlite.org/2024/sqlite-autoconf-3470000.tar.gz"
-        end
-        set_version(version)
-        set_urls(download_url, {version = function (version)
-          local version_str = version:gsub("[.+]", "")
-          if #version_str < 7 then
-              version_str = version_str .. "00"
-          end
-          return version_str
-        end})
-        
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
         for opt, value in pairs(package:configs()) do
