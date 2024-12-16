e7ae665755780334aab471b0bdff7cbd3cf9bbf8
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-16 21:59:01 +0800

    Update xmake.lua

--- packages/c/c-ares/xmake.lua
+++ packages/c/c-ares/xmake.lua
@@ -1,50 +1,38 @@
-package("sqlite3")
-    set_homepage("https://sqlite.org/index.html")
-    set_description("SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine.")
+package("c-ares")
+    set_homepage("https://c-ares.org")
+    set_description("A C library for asynchronous DNS requests")
     set_license("MIT")
-
-    set_urls("https://sqlite.org/$(version)", {version = function (version)
-        local year = "2024"
-        if version:le("3.24") then
-            year = "2018"
-        elseif version:le("3.36") then
-            year = "2021"
-        elseif version:le("3.42") then
-            year = "2022"
-        elseif version:le("3.44") then
-            year = "2023"
-        end
-        local version_str = version:gsub("[.+]", "")
-        if #version_str < 7 then
-            version_str = version_str .. "00"
-        end
-        return year .. "/sqlite-autoconf-" .. version_str .. ".tar.gz"
-    end})
+    set_urls("https://github.com/c-ares/c-ares/releases/download/v$(version)/c-ares-$(version).tar.gz")
 
     --insert version
-    add_versions("3.47.0+200", "52cd4a2304b627abbabe1a438ba853d0f6edb8e2774fcb5773c7af11077afe94")
-    add_versions("3.40.0+0", "0333552076d2700c75352256e91c78bf5cd62491589ba0c69aed0a81868980e7")
-
-    add_configs("explain_comments", { description = "Inserts comment text into the output of EXPLAIN.", default = true, type = "boolean"})
-    add_configs("dbpage_vtab",      { description = "Enable the SQLITE_DBPAGE virtual table.", default = true, type = "boolean"})
-    add_configs("stmt_vtab",        { description = "Enable the SQLITE_STMT virtual table logic.", default = true, type = "boolean"})
-    add_configs("dbstat_vtab",      { description = "Enable the dbstat virtual table.", default = true, type = "boolean"})
-    add_configs("math_functions",   { description = "Enable the built-in SQL math functions.", default = true, type = "boolean"})
-    add_configs("rtree",            { description = "Enable R-Tree.", default = false, type = "boolean"})
-    add_configs("safe_mode",        { description = "Use thread safe mode in 0 (single thread) | 1 (serialize) | 2 (mutli thread).", default = "1", type = "string", values = {"0", "1", "2"}})
-    add_configs("cli",              { description = "Build the sqlite3 command line shell.", default = false, type = "boolean"})
-
-    on_install(function (package)
-        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        local configs = {}
-        for opt, value in pairs(package:configs()) do
-            if not package:extraconf("configs", opt, "builtin") then
-                configs[opt] = value
-            end
+    add_versions("1.34.3", "26e1f7771da23e42a18fdf1e58912a396629e53a2ac71b130af93bbcfb90adbe")
+    on_load(function (package) 
+        if package:is_plat("windows", "mingw") and package:config("shared") ~= true then
+            package:add("defines", "CARES_STATICLIB")
         end
+    end)
+    on_install(function (package)
+        local transforme_configfile = function (input, output) 
+            output = output or input
+            local lines = io.readfile(input):gsub("@([%w_]+)@", "${%1}"):split("\n")
+            local out = io.open(output, 'wb')
+            for _, line in ipairs(lines) do
+                if line:startswith("#cmakedefine") then
+                    local name = line:split("%s+")[2]
+                    line = "${define "..name.."}"
+                end
+                out:write(line)
+                out:write("\n")
+            end
+            out:close()
+        end
+        transforme_configfile("include/ares_build.h.cmake", "cmake/ares_build.h.in")
+        transforme_configfile("src/lib/ares_config.h.cmake", "cmake/ares_config.h.in")
+        os.cp(path.join(os.scriptdir(), "port/*.lua"), "./")
+        local configs = {}
         import("package.tools.xmake").install(package, configs)
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("sqlite3_libversion_number()", {includes = "sqlite3.h"}))
+        assert(package:has_cfuncs("ares_library_initialized", {includes = {"ares.h"}}))
     end)
