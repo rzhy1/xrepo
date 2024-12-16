9cd76273e197c07392d869e91fa925f30d1de680
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-12-16 12:46:47 +0800

    Update xmake.lua

--- packages/e/expat/xmake.lua
+++ packages/e/expat/xmake.lua
@@ -1,18 +1,16 @@
+
 package("expat")
     set_homepage("https://libexpat.github.io")
     set_description("expat is a stream-oriented XML parser library written in C.")
     set_license("MIT")
+    set_urls("https://github.com/libexpat/libexpat/releases/download/R_$(version).tar.bz2", {version = function (version)
+        return version:gsub("%.", "_") .. "/expat-" .. version
+    end})
 
-    -- 禁用哈希值校验
-    set_urls("https://github.com/libexpat/libexpat/releases/download/R_$(version).tar.bz2", {
-        version = function (version)
-            return version:gsub("%.", "_") .. "/expat-" .. version
-        end,
-        verify = false -- 添加此项以跳过哈希校验
-    })
-
-    -- 插入版本（无哈希值）
-    add_versions("2.6.4")
+    --insert version
+    add_versions("2.6.4", "8dc480b796163d4436e6f1352e71800a774f73dbae213f1860b60607d2a83ada")
+    add_versions("2.6.3", "b8baef92f328eebcf731f4d18103951c61fa8c8ec21d5ff4202fb6f2198aeb2d")
+    add_versions("2.6.2", "9c7c1b5dcbc3c237c500a8fb1493e14d9582146dd9b42aa8d3ffb856a3b927e0")
 
     on_load(function (package)
         if package:config("shared") ~= true then
