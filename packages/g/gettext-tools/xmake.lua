3a5f493252471cde8bbc25bd9061e33c097208c7
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-08-15 22:20:21 +0800

    Update xmake.lua

--- packages/g/gettext-tools/xmake.lua
+++ packages/g/gettext-tools/xmake.lua
@@ -4,7 +4,6 @@ package("gettext-tools")
     set_description("GNU gettext tools compiled binaries for Windows")
     set_license("GPL-3.0")
     set_urls("https://github.com/vslavik/gettext-tools-windows/releases/download/v$(version)/gettext-tools-windows-$(version).zip")
-              https://github.com/vslavik/gettext-tools-windows/releases/download/v0.26/gettext-tools-windows-0.26.zip
 
     --insert version
     add_versions("0.26", "31b0d12d16f4e6655bb4922332f931d69a2e105d17c5e2ebadc7a5b0735d37ff")
