2c8f81b88329e9895d064367dd1e6cc8cb73e468
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-11-05 22:33:07 +0800

    Update xmake.lua

--- packages/q/quictls/xmake.lua
+++ packages/q/quictls/xmake.lua
@@ -1,16 +1,19 @@
-package("quictls")
-    set_homepage("https://quictls.github.io/openssl")
-    set_description("TLS/SSL and crypto library with QUIC APIs")
+package("openssl")
+    set_homepage("https://www.openssl.org/")
+    set_description("TLS/SSL and crypto library")
     set_license("Apache-2.0")
-    set_urls("https://github.com/quictls/openssl/archive/refs/tags/openssl-$(version).tar.gz")
+    set_urls("https://github.com/openssl/openssl/releases/download/openssl-$(version)/openssl-$(version).tar.gz")
 
-    --insert version
-    add_versions("3.3.0-quic1", "78d675d94c0ac3a8b44073f0c2b373d948c5afd12b25c9e245262f563307a566")
+    -- 添加版本信息
+    add_versions("3.6.0", "b6a5f44b7eb69e3fa35dbf15524405b44837a481d43d81daddde3ff21fcbb8e9")
+    
     add_configs("installdir", {description = "installdir set", default = nil, type = "string"})
     add_links("ssl", "crypto")
+    
     if is_plat("windows", "mingw") then
         add_syslinks("user32", "ws2_32", "advapi32", "crypt32")
     end
+
     on_install(function (package)
         os.cp(path.join(os.scriptdir(), "port", "generate_xmake.lua"), "xmake.lua")
         os.cp(path.join(os.scriptdir(), "port", "scripts"), "scripts")
