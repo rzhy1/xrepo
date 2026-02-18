38d4d57cfa68ff358a5937eaa95f99db4cb6593c
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-02-18 09:22:50 +0800

    Update xmake.lua

--- packages/z/zlib/xmake.lua
+++ packages/z/zlib/xmake.lua
@@ -5,9 +5,7 @@ package("zlib")
     set_urls("https://github.com/madler/zlib/releases/download/v$(version)/zlib-$(version).tar.gz")
 
     --insert version
-    add_versions("1.3.1", "9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23")
-    add_versions("1.3", "ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e")
-    add_versions("1.2.13", "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30")
+    add_versions("1.3.2", "bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16")
 
     add_includedirs("include")
     add_includedirs("include/zlib")
