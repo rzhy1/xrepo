f0f0f82b17a2243b63d45c7a44eecabd298cce9a
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-06-27 12:51:03 +0800

    Update xmake.lua

--- packages/s/sqlite3/xmake.lua
+++ packages/s/sqlite3/xmake.lua
@@ -19,7 +19,7 @@ package("sqlite3")
         end
     end})
 
-    add_versions("3.53.0+2026.200", "588ad51949419a56ebe81fe56193d510c559eb94c9a57748387860b5d3069316")
+    add_versions("3.53.0+2026.300", "c917d7db16648ec95f714974ace5e5dcf46b7dc70e26600a0a102a3141125db0")
 
     add_configs("explain_comments", { description = "Inserts comment text into the output of EXPLAIN.", default = true, type = "boolean"})
     add_configs("dbpage_vtab",      { description = "Enable the SQLITE_DBPAGE virtual table.", default = true, type = "boolean"})
